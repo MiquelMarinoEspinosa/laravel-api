6d2cf16eaa22310a423190bc7702a4e8265b8b2e
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2023-09-22 10:53:01 +0200

    register users and create tokens

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,6 +21,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/products/{id}', [ProductController::class, 'show']);
 //Public routes
 //Route::resource('products', ProductController::class);
+Route::post('/register', [AuthController::class, 'register']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
