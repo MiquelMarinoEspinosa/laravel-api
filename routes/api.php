8a8940e43ab7741b8b16194a15b407afce6f0355
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2023-09-22 10:35:16 +0200

    protecting routes

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,16 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/products', [ProductController::class, 'index']);
 // Route::post('/products', [ProductController::class, 'store']);
 // Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::resource('products', ProductController::class);
+//Public routes
+//Route::resource('products', ProductController::class);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
+
+//Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/products/search/{name}', [ProductController::class, 'search']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
